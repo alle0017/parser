31abf8e33ef8fa4323b2f7472e53b418a2ae094d
alle0017 <allegretti0017@gmail.com>
2026-04-21 10:06:04 +0200

    implemented dominance tree and dominance frontier search

--- src/dataflow/dt_operator.ts
+++ src/dataflow/dt_operator.ts
@@ -1,6 +1,8 @@
 import { BasicBlock } from '../ast/basic_block.ts';
 import { Ir } from '../ast/ir.ts';
+import { DataflowAnalyzer } from './dataflow_analyzer.ts';
 import { FlowOperator } from './flow_operator.ts';
+import { Node } from "../utils/node.ts"
 
 /**
  * dominance tree operator for forward analysis
@@ -24,5 +26,49 @@ export class DTOperator implements FlowOperator<Set<BasicBlock<Ir>>> {
       }
       changed(current: Set<BasicBlock<Ir>>, previous: Set<BasicBlock<Ir>>): boolean {
             return current.size != previous.size;
+      }
+}
+
+export class DominanceTree {
+      private readonly dominators: Map<BasicBlock<Ir>, Set<BasicBlock<Ir>>>;
+      constructor(private readonly blocks: BasicBlock<Ir>[]) {
+            const {output} = new DataflowAnalyzer<Set<BasicBlock<Ir>>>(blocks).forwardAnalysis(new DTOperator(blocks), new Set([blocks[0]]));
+            this.dominators = output;
+      }
+
+      private buildTree(current: BasicBlock<Ir>, dominators:  Map<BasicBlock<Ir>, Set<BasicBlock<Ir>>>, seen: Set<BasicBlock<Ir>>) {
+            const node = new Node(current, []);
+            
+            for (const dom of current.next) {
+                  if (!seen.has(dom)) {
+                        seen.add(dom);
+                        node.append(this.buildTree(dom, dominators, seen));
+                  }
+            }
+            return node;
+      }   
+      public getDominanceFrontierOf(node: BasicBlock<Ir>) {
+            const df: Set<BasicBlock<Ir>> = new Set();
+            const seen: Set<BasicBlock<Ir>> = new Set();
+            const stack = [node];
+
+            while (stack.length > 0) {
+                  const curr = stack.pop()!;
+
+                  if (this.dominators.get(curr)?.has(node)) {
+                        for (const nn of curr.next) {
+                              if (!seen.has(nn)) {
+                                    seen.add(nn);
+                                    stack.push(nn);
+                              }
+                        }
+                  } else {
+                        df.add(curr);
+                  }
+            }
+            return df;
+      }
+      public get() {
+            return this.buildTree(this.blocks[0], this.dominators, new Set());
       }
 }
