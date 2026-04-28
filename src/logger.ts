f406486a97cdeb3505a396b6bef139d619cf1f43
alle0017 <allegretti0017@gmail.com>
2026-04-28 15:47:00 +0200

    logger - implemented logger

---
+++ src/logger.ts
@@ -0,0 +1,33 @@
+const LogConstructor = (name: string) => new Logger(name);
+export class Logger {
+      private static readonly loggers: Map<string,Logger> = new Map();
+      static getLogger(name: string) {
+            return this.loggers.getOrInsertComputed(name, LogConstructor);
+      }
+
+      static enable(name: string) {
+            const logger = this.loggers.getOrInsertComputed(name, LogConstructor);
+            logger.enableLog();
+
+            for (const hs of logger.history) {
+                  console.log(hs);
+            }
+      }
+      private readonly history: string[] = [];
+      private enabled: boolean = false;
+      constructor(private readonly name: string) {}
+
+      public enableLog() {
+            this.enabled = true;
+            return this;
+      }
+      public log(...values: unknown[]) {
+            const now = performance.now();
+
+            this.history.push(`[${this.name};${now}] ${values.map(v => v?.toString()).join('')}`);
+
+            if (this.enabled) {
+                  console.log(this.history.at(-1)!);
+            }
+      }
+}
